4a85cab26197a22e49c1a22a3c628a28a2ec9e14
David Hays <dhays90@gmail.com>
2013-11-15 19:29:47 -0600

    Recovery not support metadata
    Needs http://review.cyanogenmod.org/#/c/53559/
    
    Remove swipe features. It acts crazy!

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -90,4 +90,4 @@ TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storag
 TARGET_RECOVERY_FSTAB := device/htc/vigor/rootdir/etc/fstab.vigor
 BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_15x24.h\"
-BOARD_RECOVERY_SWIPE := true
+USE_SET_METADATA := false
