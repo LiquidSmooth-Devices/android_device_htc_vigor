a5c79d18ba3097a7a9542d0b422080778200d604
Jordan <Bunchies101@users.noreply.github.com>
2014-05-17 11:07:36 -0700

    Update liquid.mk
    rearrange just in case
    http://review.cyanogenmod.org/#/c/59655/

--- liquid.mk
+++ liquid.mk
@@ -1,5 +1,5 @@
-$(call inherit-product, device/htc/vigor/vigor.mk)
 $(call inherit-product, vendor/liquid/config/common_phone.mk)
+$(call inherit-product, device/htc/vigor/vigor.mk)
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
